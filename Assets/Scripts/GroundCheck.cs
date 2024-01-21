ec9c480d0e858eb683b00358a2b0b4d65a60d508
AnnaDegtyareva <89099557+AnnaDegtyareva@users.noreply.github.com>
2024-01-21 13:17:30 +0500

    fix

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -8,7 +8,6 @@ public class GroundCheck : MonoBehaviour
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        PM.isGround = true;
         if (collision.gameObject.tag != "Player")
         {
             PM.isGround = true;
@@ -16,7 +15,6 @@ public class GroundCheck : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        PM.isGround = false;
         if (collision.gameObject.tag != "Player")
         {
             PM.isGround = false;
