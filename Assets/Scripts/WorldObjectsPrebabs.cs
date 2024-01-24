d85e1f016a99293c2fb31303328197dbe3f2f1a2
1mpulseee <83034758+1mpulseee@users.noreply.github.com>
2024-01-24 10:38:57 +0300

    массив объектов

---
+++ Assets/Scripts/WorldObjectsPrebabs.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "WorldObjectsPrebabs", menuName = "Game/New WorldObjectsPrebabs")]
+public class WorldObjectsPrebabs : ScriptableObject
+{
+    [System.Serializable]
+    public class platforms
+    {
+        [System.Serializable]
+        public class type
+        {
+            [System.Serializable]
+            public class item
+            {
+                public GameObject Prefab;
+                public int weight = 1;
+            }
+            public item[] size3x2;
+            public item[] size5x2;
+            public item[] size5x5;
+            public item[] size7x2;
+            public item[] size7x5;
+            public item[] size9x3;
+        }
+        public type[] Types;
+    }
+    [System.Serializable]
+    public class props
+    {
+        [System.Serializable]
+        public class type
+        {
+
+        }
+        public type[] Types;
+    }
+
+    public platforms Platforms;
+    public props Props;
+}
