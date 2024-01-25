b1cea3ffdc2f97c27209bc475a6cae5d410eea6e
AnnaDegtyareva <89099557+AnnaDegtyareva@users.noreply.github.com>
2024-01-25 16:18:24 +0500

    fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Realtime;
 using Photon.Pun;
+using TMPro;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
     public static GameManager instance;
     public WorldObjectsPrebabs WorldPrefabs;
     [SerializeField] GameObject PlayerPrefab;
+    [SerializeField] TextMeshProUGUI textPlayers;
     private void Awake()
     {
         instance = this;
@@ -16,5 +18,16 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         GameObject player = PhotonNetwork.Instantiate(PlayerPrefab.name, transform.position, transform.rotation);
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        //RefreshPlayers();
+        
+        textPlayers.text = "Player " + newPlayer.NickName + " joing room";
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        //RefreshPlayers();
+        textPlayers.text = "Player " + otherPlayer.NickName + " left room";
     }
 }
